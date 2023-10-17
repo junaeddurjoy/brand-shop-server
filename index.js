33aafa43f48e131367247d157aaceb14756e1332
junaeddurjoy <junaed.durjoy@gmail.com>
2023-10-17 19:27:15 +0600

    database secured

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// brand-shop
-// HmmCXRBMeTU4PA3w
-
-
-const uri = "mongodb+srv://brand-shop:HmmCXRBMeTU4PA3w@cluster0.schfv1q.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.schfv1q.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,7 +28,6 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
