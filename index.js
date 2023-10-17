84acd774a07f2fe2e0840058a549b626680ecb50
junaeddurjoy <junaed.durjoy@gmail.com>
2023-10-17 19:19:23 +0600

    database created

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// brand-shop
+// HmmCXRBMeTU4PA3w
+
+
+const uri = "mongodb+srv://brand-shop:HmmCXRBMeTU4PA3w@cluster0.schfv1q.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/',(req,res) => {
     res.send('server side running')
@@ -13,3 +43,4 @@ app.get('/',(req,res) => {
 app.listen(port, ()=> {
     console.log(`server loaded ${port}`)
 })
+
